24d10b0e7f2b3dd699866140e88fe6c73359701e
fathimathasneem96 <47521037+fathimathasneem96@users.noreply.github.com>
2025-04-23 16:06:50 +0530

    Update regx.py

--- 2025-04-23/regx.py
+++ 2025-04-23/regx.py
@@ -1,3 +1,4 @@
+#search for match
 import re
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt)
@@ -5,21 +6,25 @@ if x:
   print("YES! We have a match!")
 else:
   print("No match")
+Output:YES! We have a match!
 
-
+#search for price
 import re
 text = "The price is $45.50 and the ID is ID1234."
 # Find price
 price = re.search(r"\$\d+\.\d{2}", text)
-print(price.group())  # $45.50
+print(price.group())  
+Output:$45.50
 
 # Find all uppercase letter words
 words = re.findall(r"\b[A-Z]+\b", text)
-print(words)  # ['ID']
+print(words)  
+Output:['ID']
 
 # Replace dollar sign with "USD"
 updated = re.sub(r"\$", "USD ", text)
-print(updated)  # The price is USD 45.50 and the ID is ID1234.
+print(updated) 
+Output:The price is USD 45.50 and the ID is ID1234.
 
 
 #Dates
@@ -27,20 +32,24 @@ import re
 pattern = r"\b\d{2}[-/]\d{2}[-/]\d{4}\b"
 text = "Today's date is 23/04/2025, and yesterday was 22-04-2025.Tomorrow is 2025/04/24"
 matches = re.findall(pattern, text)
-print(matches)  # ['23/04/2025', '22-04-2025']
+print(matches)  
+Output:['23/04/2025', '22-04-2025']
 
 pattern = r"\b\d{4}-\d{2}-\d{2}\b"
 text = "Deadline: 2025-04-23."
-print(re.findall(pattern, text))  # ['2025-04-23']
+print(re.findall(pattern, text))  
+Output:['2025-04-23']
 
 pattern = r"\b(?:January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2}, \d{4}\b"
 text = "The event is on April 23, 2025."
-print(re.findall(pattern, text))  # ['April 23, 2025']
+print(re.findall(pattern, text)) 
+Output:['April 23, 2025']
 
 #Phone number
 pattern = r"\b\d{10}\b"
 text = "My backup number is 9876543210."
-print(re.findall(pattern, text))  # ['9876543210']
+print(re.findall(pattern, text))  
+Output:['9876543210']
 
 #Email
 import re
@@ -84,3 +93,35 @@ Output: True
 True
 False
 
+
+#search
+import re
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+Output:<re.Match object; span=(5, 7), match='ai'>
+
+
+#match.span()
+import re
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+Output:(12, 17)
+
+
+#match.string()
+import re
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
+Output:The rain in Spain
+
+
+#match.group()
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
+Output:Spain
